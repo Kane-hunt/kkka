afae8308ccd936b39beded354d0f5489c236d4ec
kane <kane@LAPTOP-7I0BC3GL>
2021-11-07 16:55:26 +0800

    添加项目文件。

---
+++ kkka/源.cpp
@@ -0,0 +1,97 @@
+#include<stdio.h>
+int main() {
+	int max();
+	int at;
+	at = max();
+	/*int tat, a, b,c,min=0,max=0;
+	printf("请输入a和b的值\n");
+	tat = scanf_s("%d%d%d",&a,&b,&c);
+	//printf("%d\n", tat);
+	printf("\n%d,%d\n", a, b);
+	/*if (a<b) {
+		if (a<c) {
+			printf("min=%d", a);
+		}else {
+			printf("min=%d", c);
+		}
+		if (b< c) {
+			printf("max=%d", c);
+		}
+		else {
+			printf("max=%d", b);
+		}
+	}else {
+		if (b < c) {
+			printf("min=%d", b);
+		}
+		else {
+			printf("min=%d", c);
+		}
+		if (a < c) {
+			printf("max=%d", c);
+		}
+		else {
+			printf("max=%d", a);
+		}
+	}*/
+	/*if (a < b) {
+		min = a;
+		if (min > c) {
+			min = c;
+		}
+		max = b;
+		if (max < c) {
+			max = c;
+		}
+	}else {
+		min = b;
+		if (min > c) {
+			min = c;
+		}
+		max = a;
+		if (max < c) {
+			max = c;
+		}
+	}
+	printf("min=%d\nmax=%d", min, max);*/
+	/*max = a;
+	if (max<b) {
+		max = b;
+		if (max < c) {
+			max = c;
+		}
+	}else{
+		if (max<c) {
+			max = c;
+		}
+	}
+	printf("max=%d", max);*/
+	/*max = a / 10;
+	printf("%d", max);
+	switch (max)
+	{
+	case 10:
+		printf("满分");
+		break;
+	case 9:
+	case 8:
+		printf("优秀");
+		break;
+	case 7:
+	case 6:
+		printf("及格");
+		break;
+	case 5:
+	case 4:
+	case 3:
+	case 2:
+	case 1:e
+	case 0:
+		printf("不及格");
+		break;
+	default:
+		printf("格式错误！");
+		break;
+	}*/
+	//return 0;
+}
